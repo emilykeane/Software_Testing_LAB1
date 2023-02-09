fdd5f44f8caed9f9e90eeb53f79cf5b437a473c1
Cáhan Brolly <cbrolly07@qub.ac.uk>
2023-02-09 11:17:06 +0000

    cahan part

--- Mini-lab Java Refresher 1-Simple banking app/tests/UserTests.java
+++ Mini-lab Java Refresher 1-Simple banking app/tests/UserTests.java
@@ -7,9 +7,22 @@ public class UserTests {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		User testUser = new User("mike", "my_passwd", "Mike", "Smith", "07771234567");
+		String test_username = "mike";
+		String test_password = "my_passwd";
+		String test_first_name = "Mike";
+		String test_last_name = "Smith";
+		String test_mobile_number = "07771234567";
+		User testUser = new User(test_username, test_password,test_first_name , test_last_name, test_mobile_number);
 		
 		System.out.println(testUser);
+		
+		assert testUser.getUsername() == test_username;
+		assert testUser.getPassword() == test_password;
+		assert testUser.getFirst_name() == test_first_name;
+		assert testUser.getLast_name() == test_last_name;
+		assert testUser.getMobile_number() == test_mobile_number;
+		System.out.println("A");
+		assert 1==2;
 		
 	}
 
